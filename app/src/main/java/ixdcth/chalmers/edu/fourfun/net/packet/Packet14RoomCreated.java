cb9ae2cd2c30b1557ee820734cfa01258113676a
Simon Eliasson <digohd1337@gmail.com>
2015-12-10 16:44:49 +0100

    RoomCreated PAcket has Color

--- app/src/main/java/ixdcth/chalmers/edu/fourfun/net/packet/Packet14RoomCreated.java
+++ app/src/main/java/ixdcth/chalmers/edu/fourfun/net/packet/Packet14RoomCreated.java
@@ -4,29 +4,33 @@ import ixdcth.chalmers.edu.fourfun.net.Server;
 
 public class Packet14RoomCreated extends Packet{
 
+    String hexColor;
+
     public Packet14RoomCreated(byte[] data) {
         super(14);
         String[] dataArray = readData(data).split(":");
+        this.hexColor = dataArray[1];
     }
 
-    public Packet14RoomCreated(String username) {
+    public Packet14RoomCreated(String hexColor) {
         super(14);
+        this.hexColor = hexColor;
     }
 
     @Override
     public void writeData(Server server) {
-        
+
     }
 
     @Override
     public byte[] getData() {
-        return ("14").getBytes();
+        return ("14" + ":" + hexColor).getBytes();
     }
 
-	@Override
-	public void writeData(Client client) {
-		
-	}
+    @Override
+    public void writeData(Client client) {
+
+    }
 
 
 }
